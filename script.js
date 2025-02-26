6ff796969368716c6172f6e578a2c8f58a20b712
fluecubic <jonas.lorenz1234567890@gmail.com>
2025-02-26 09:15:40 +0100

    ddfdf

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let outputElement = document.getElementById("outputxd");
 let lastnumber = 1;
 
 setInterval(function () {
-    outputElement.innerHTML += count+". "lastnumber * 2 + "<br>";
+    outputElement.innerHTML += count+". "+lastnumber * 2 + "<br>";
     lastnumber = lastnumber * 2;
 }, 100);
 
