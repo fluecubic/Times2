a0ceefb727b1f572aefe676b95f4f6a071baefa0
fluecubic <jonas.lorenz1234567890@gmail.com>
2025-02-26 09:11:29 +0100

    gggg

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 window.location.replace("./#last")
-
+let count = 1;
 let outputElement = document.getElementById("outputxd");
 let lastnumber = 1;
 
 setInterval(function () {
-    outputElement.innerHTML += lastnumber * 2 + "<br>";
+    outputElement.innerHTML += count+". "lastnumber * 2 + "<br>";
     lastnumber = lastnumber * 2;
 }, 100);
 
